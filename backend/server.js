b26e45f863f5ad2a5aa7e737593e390c8fc2e7d1
QhayiyaVenkile <qhayiyavenks@gmail.com>
2023-10-06 06:36:21 +0200

    added database functionality

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 // UUID imports
 const crypto = require("crypto");
 
+
 // encrypt password imports
 const bcrypt = require("bcrypt");
 
@@ -20,11 +21,81 @@ app.use(cors())
 
 // mySQL API connection
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "db",
     user: 'root',
-    password: '',
+    password: 'root123',
     database: 'duewallet'
 })
+
+db.connect((err) => {
+    if (err) {
+        console.log("Database Connection Failed", err);
+    } else {
+        console.log("Connected to Database");
+        console.log("Creating database table")
+        let tableName = 'users';
+
+        // query to create table users
+        let query = `CREATE TABLE ${tableName} 
+        (
+            "UserID" VARCHAR(36) PRIMARY KEY,
+            "UserName" VARCHAR(64) NOT NULL,
+            "Email" VARCHAR(254) NOT NULL,
+            "Password" VARCHAR(255) NOT NULL,
+            "CreatedAt" DATE NOT NULL
+        )`;
+
+        db.query(query, (err, rows) => {
+            if (err) {
+                console.log("Table Exist");
+            } else {
+                console.log(`Successfully Created Table - ${tableName}`)
+            }
+        })
+
+        console.log("Creating database table")
+        let tableName1 = 'budgets';
+
+        // query to create table budgets
+        let query1 = `CREATE TABLE ${tableName1} 
+        (
+            "BudgetID" VARCHAR(36) PRIMARY KEY,
+            "BudgetName" VARCHAR(64) NOT NULL,
+            "Allocated" DECIMAL(15, 2) NOT NULL,
+            "CreatedAt" DATE NOT NULL
+        )`;
+
+        db.query(query1, (err, rows) => {
+            if (err) {
+                console.log("Table Exist");
+            } else {
+                console.log(`Successfully Created Table - ${tableName1}`)
+            }
+        })
+
+        console.log("Creating database table")
+        let tableName2 = 'expenses';
+
+        // query to create table expenses
+        let query2 = `CREATE TABLE ${tableName2} 
+        (
+            "ExpenseID" VARCHAR(36) PRIMARY KEY,
+            "ExpenseName" VARCHAR(64) NOT NULL,
+            "Amount" DECIMAL(15, 2) NOT NULL,
+            "CreatedAt" DATE NOT NULL,
+            "BudgetID" VARCHAR(36) NOT NULL,
+            FOREIGN KEY(BudgetID) REFERENCES budgets(BudgetID)
+        )`;
+
+        db.query(query2, (err, rows) => {
+            if (err) {
+                console.log("Table Exist");
+            } else {
+                console.log(`Successfully Created Table - ${tableName2}`)
+            }
+        })
+    }
+});
 
 // insert into table users
 app.post('/users', async (req, res) => {
